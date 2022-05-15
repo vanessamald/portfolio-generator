eeb0f1d066012556436303f7a21b4d6bc9680af7
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-05-15 13:21:09 -0500

    creating header and footer

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('src/page-template.js');
-
-//const pageHTML = generatePage(name, github);
-
-//fs.writeFile('index.html', generatePage(name, github), err => {
-//    if (err) throw err;
-  
-//    console.log('Portfolio complete! Check out index.html to see the output!');
-//  });
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
   
@@ -114,5 +106,12 @@ const promptProject = portfolioData => {
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('index.html', pageHTML, err => {
+    if (err) throw Error(err);
+  
+    console.log('Portfolio complete! Check out index.html in this directory to see it!');
+  });
+
   });
